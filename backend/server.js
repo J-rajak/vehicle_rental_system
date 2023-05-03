59988740ef86d4ac8d403c148675ce72288a3c1d
J-rajak <117970877+J-rajak@users.noreply.github.com>
2023-05-03 15:39:44 +0545

    deploy

--- backend/server.js
+++ backend/server.js
@@ -89,7 +89,7 @@ app.use("/api", apiRoutes);
 const path = require("path");
 if (process.env.NODE_ENV === "production") {
     app.use(express.static(path.join(__dirname, "../frontend/build")));
-    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "build", "index.html")));
+    app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "../frontend", "public", "index.html")));
 } else {
     app.get("/", (req, res) => {
         res.json({ message: "API running..." });
